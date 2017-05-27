6d7969035b1c3be40120693cbb8d8630e210535d
zhentoufei <zhentoufei@163.com>
2017-05-27 22:48:20 +0800

    看了add two number的题目，更新了一个新方法

--- Python/Add Two Numbers/Add_Two_Numbers.py
+++ Python/Add Two Numbers/Add_Two_Numbers.py
@@ -7,6 +7,25 @@ You may assume the two numbers do not contain any leading zero, except the numbe
 
 Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
+下面的解法来自：http://bookshadow.com/weblog/2015/04/05/leetcode-add-two-numbers/
+def addTwoNumbers(self, l1, l2):
+        head = ListNode(0)
+        l = head
+        carry = 0
+        while l1 or l2 or carry:
+            sum, carry = carry, 0
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+            if sum > 9:
+                carry = 1
+                sum -= 10
+            l.next = ListNode(sum)
+            l = l.next
+        return head.next
 '''
 class ListNode:
     def __init__(self, x):
