b0cdbbf457c85b8ac161d74eaf425220fb0e8594
zhentoufei <zhentoufei@163.com>
2017-07-17 19:45:37 +0800

    change name of directories

---
+++ Python/3. Longest Substring Without Repeating Characters/lengthOfLongestSubstring.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+
+# 第一种方法
+def lengthOfLongestSubstring(self, s):
+    start = 0
+    maxlen = 0
+    hashtable = [-1 for i in range(256)]
+    for i in range(len(s)):
+        if hashtable[ord(s[i])] != -1:
+            while start <= hashtable[ord(s[i])]:
+                hashtable[ord(s[start])] = -1
+                start += 1
+        if i - start + 1 > maxlen:
+            maxlen = i - start + 1
+        hashtable[ord(s[i])] = i
+    return maxlen
+
+
+# 第二种方法
+def lengthOfLongestSubstring1(s):
+    start = 0
+    maxlen = 0
+    dict = {}
+    for i in range(len(s)):
+        dict[s[i]] = -1
+    for i in range(len(s)):
+        if dict[s[i]] != -1:
+            while start <= dict[s[i]]:
+                dict[s[start]] = -1
+                start += 1
+        if i - start + 1 > maxlen:
+            maxlen = i - start + 1
+        dict[s[i]] = i
+    return maxlen
+
+if __name__=='__main__':
+    s = '123112'
+    print lengthOfLongestSubstring1(s)
---
+++ Python/BinarySearch/binarySearch.py
@@ -0,0 +1,30 @@
+# coding:utf8
+def binary_search(lst, value, low, high):  # low,high是lst的查找范围
+    if high < low:
+        return -1
+    mid = (low + high) / 2  # 在此处自动向下取整
+    if lst[mid] > value:
+        return binary_search(lst, value, low, mid - 1)
+    elif lst[mid] < value:
+        return binary_search(lst, value, mid + 1, high)
+    else:
+        return mid
+
+
+def b_search(l, value):
+    lo, hi = 0, len(l) - 1
+    while lo <= hi:
+        mid = (lo + hi) / 2
+        if l[mid] < value:
+            lo = mid + 1
+        elif value < l[mid]:
+            hi = mid - 1
+        else:
+            return mid
+    return -1
+
+
+if __name__ == '__main__':
+    l = range(50)
+    print binary_search(l, 10, 0, 49)
+    print b_search(l, 10)
