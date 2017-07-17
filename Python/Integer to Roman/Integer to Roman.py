81c45ec8eb2f6516f0986b349c57248890b1ad2b
zhentoufei <zhentoufei@163.com>
2017-07-17 17:09:47 +0800

    Integer to Roman

---
+++ Python/Integer to Roman/Integer to Roman.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+# Time:  O(n)
+# Space: O(1)
+#
+# Given an integer, convert it to a roman numeral.
+#
+# Input is guaranteed to be within the range from 1 to 3999.
+#
+# 关于问题的详细解释可以见http://www.cnblogs.com/grandyang/p/4120857.html
+class Solution:
+    # @return a string
+    def intToRoman(self, num):
+        numeral_map = {1: "I", 4: "IV", 5: "V", 9: "IX",
+                       10: "X", 40: "XL", 50: "L", 90: "XC",
+                       100: "C", 400: "CD", 500: "D", 900: "CM",
+                       1000: "M"}
+        key_set, result = sorted(numeral_map.keys()), ""
+        print key_set
+
+        while num > 0:
+            for key in reversed(key_set):
+                while num / key > 0:
+                    num -= key
+                    result += numeral_map[key]
+
+        return result
+
+
+if __name__ == "__main__":
+    print Solution().intToRoman(999)
+    print Solution().intToRoman(3999)
