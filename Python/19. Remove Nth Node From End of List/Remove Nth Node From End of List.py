8fa178c5dda151295ace056dd2b9bc49d13abdb1
zhentoufei <zhentoufei@163.com>
2017-07-19 15:07:39 +0800

    add a new method to removeNthFromEnd

--- Python/19. Remove Nth Node From End of List/Remove Nth Node From End of List.py
+++ Python/19. Remove Nth Node From End of List/Remove Nth Node From End of List.py
@@ -44,11 +44,35 @@ class Solution:
 
         while fast.next:
             slow, fast = slow.next, fast.next
+            print slow
 
         slow.next = slow.next.next
 
         return dummy.next
 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution2(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        fast = slow = head
+        for _ in range(n):
+            fast = fast.next
+        if not fast:
+            return head.next
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return head
 
 if __name__ == "__main__":
     head = ListNode(1)
