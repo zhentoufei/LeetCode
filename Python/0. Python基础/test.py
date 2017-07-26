a27a5ca4db76aa05d607dafc7500b325bafdc6e7
Mr.Finger <zhentoufei@163.com>
2017-07-26 14:39:03 +0800

    method caller

--- Python/0. Python基础/test.py
+++ Python/0. Python基础/test.py
@@ -1,19 +1,9 @@
 # coding:utf-8
-from itertools import chain
-from random import randint
-# 例子1
-for x in chain([1, 2, 3, 4], ['a', 'b', 'c']):
-    print x
-    # 输出1 2 3 4 a b c
 
+# 使用标准库operator下的methodcaller函数调用
+from operator import methodcaller
 
-e1 = [randint(60, 100) for _ in xrange(40)]
-e2 = [randint(60, 100) for _ in xrange(22)]
-e3 = [randint(60, 100) for _ in xrange(33)]
-e4 = [randint(60, 100) for _ in xrange(55)]
-count = 0
-for s in chain(e1, e2, e3, e4):
-    if s > 90
-        count += 1
+s = 'asdsdfgf'
+print s.find('sd', 0)
 
-print count
+print methodcaller('find', 'sd', 0)(s)
