c10dd24f266fc24b8e8ebb6d6925af35697b8a83
zhentoufei <zhentoufei@163.com>
2017-09-07 16:46:17 +0800

    记忆化搜索实现斐波那契额数列

--- Java/src/easy/fibonachi.java
+++ Java/src/easy/fibonachi.java
@@ -2,9 +2,26 @@ package easy;
 
 public class fibonachi {
 
+	public static int[] memo;
+
+	public static int fib(int n) {
+		if (n == 0)
+			return 1;
+		if (n == 1)
+			return 1;
+		if (memo[n] == -1)
+			memo[n] = fib(n - 1) + fib(n - 2);
+		return memo[n];
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int n = 4;
+		memo = new int[n + 1];
+		for (int i = 0; i < memo.length; i++) {
+			memo[i] = -1;
+		}
+		System.out.println(fib(n));
 	}
 
 }
