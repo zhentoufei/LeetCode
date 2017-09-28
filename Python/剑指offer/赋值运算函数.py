4ea7e27be31468c1d86a705cd931c85cc1b423e3
zhentoufei <zhentoufei@163.com>
2017-09-28 21:56:56 +0800

    赋值运算符

---
+++ Python/剑指offer/赋值运算函数.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Mr.Finger'
+__date__ = '2017/9/28 21:51'
+__site__ = ''
+__software__ = 'PyCharm'
+__file__ = '赋值运算函数.py'
+
+
+
+
+
+if __name__ == '__main__':
+    pass
