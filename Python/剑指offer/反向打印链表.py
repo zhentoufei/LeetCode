c0770c553463943707011e1569deb8c21ce6fdec
zhentoufei <zhentoufei@163.com>
2017-09-29 22:06:32 +0800

    输入一个链表，从尾到头打印链表每个节点的值。

---
+++ Python/剑指offer/反向打印链表.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Mr.Finger'
+__date__ = '2017/9/29 21:58'
+__site__ = ''
+__software__ = 'PyCharm'
+__file__ = '反向打印链表.py'
+
+
+class ListNode:
+    def __init__(self, x = None):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def printListFromTailToHead(self, list_node):
+        if list_node.val == None:
+            return
+        l=[]
+        head = list_node
+        while head:
+            l.insert(0,head.val)
+            head = head.next
+        return l
+
+if __name__ == '__main__':
+    node1 = ListNode(10)
+    node2 = ListNode(20)
+    node3 = ListNode(30)
+    node1.next = node2
+    node2.next = node3
+
+    singleNode = ListNode(12)
+
+    s = Solution()
+
+    print s.printListFromTailToHead(node1)
