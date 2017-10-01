14eb672dc117af3a911709d6e32879ba04f29e3e
zhentoufei <zhentoufei@163.com>
2017-10-01 22:43:29 +0800

    【test】文件夹结构调整

---
+++ Python/LeetCode/test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Mr.Finger'
+__date__ = '2017/10/1 22:43'
+__site__ = ''
+__software__ = 'PyCharm'
+__file__ = 'test.py'
+
+if __name__ == '__main__':
+    pass
