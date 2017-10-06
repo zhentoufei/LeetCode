8f8dc57eb59318520c93bdbe75c4cf42b682670f
zhentoufei <zhentoufei@163.com>
2017-10-06 17:52:59 +0800

    判断平衡二叉树

---
+++ Python/剑指offer/39.判断平衡二叉树.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Mr.Finger'
+__date__ = '2017/10/5 21:48'
+__site__ = ''
+__software__ = 'PyCharm'
+__file__ = '39.判断平衡二叉树.py'
+
+
+class TreeNode:
+    def __init__(self, x=None):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def __init__(self):
+        self.flag = True
+
+    def IsBalanced_Solution(self, pRoot):
+        self.getDepth(pRoot)
+        return self.flag
+
+    def getDepth(self, pRoot):
+        if pRoot == None:
+            return 0
+        left = 1 + self.getDepth(pRoot.left)
+        right = 1 + self.getDepth(pRoot.right)
+
+        if abs(left - right) > 1:
+            self.flag = False
+
+        return left if left > right else right
+
+
+class Solution2:
+    def getDepth(self, pRoot):
+        if pRoot == None:
+            return 0
+        return max(self.getDepth(pRoot.left), self.getDepth(pRoot.right)) + 1
+
+    def IsBalanced_Solution(self, pRoot):
+        if pRoot == None:
+            return True
+        if abs(self.getDepth(pRoot.left) - self.getDepth(pRoot.right)) > 1:
+            return False
+        return self.IsBalanced_Solution(pRoot.left) and self.IsBalanced_Solution(pRoot.right)
+
+
+if __name__ == '__main__':
+    pNode1 = TreeNode(1)
+    pNode2 = TreeNode(2)
+    pNode3 = TreeNode(3)
+    pNode4 = TreeNode(4)
+    pNode5 = TreeNode(5)
+    pNode6 = TreeNode(6)
+    pNode7 = TreeNode(7)
+
+    pNode1.left = pNode2
+    pNode1.right = pNode3
+    pNode2.left = pNode4
+    pNode2.right = pNode5
+    pNode3.right = pNode6
+    pNode5.left = pNode7
+
+    S = Solution2()
+    print(S.getDepth(pNode1))
