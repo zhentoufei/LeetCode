9f3afff25eecf0d92c69035ebec4a90680be7b3c
zhentoufei <zhentoufei@163.com>
2017-10-07 15:26:53 +0800

    update something

---
+++ Python/0. Python基础/MultiProcessing/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Mr.Finger'
+__date__ = '2017/9/11 12:24'
+__site__ = ''
+__software__ = 'PyCharm'
+__file__ = '__init__.py.py'
+
+if __name__ == '__main__':
+    pass
